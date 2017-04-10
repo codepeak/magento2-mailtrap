6092886d15d92f9c7c6b0b4414b0e23f7cd461ea
Robert Lord <robert@codepeak.se>
2017-04-10 17:35:19 +0200

    First version

---
+++ Model/Transport.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Codepeak\Mailtrap\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Mail\MessageInterface;
+
+/**
+ * Class Transport
+ *
+ * @package  Codepeak\Mailtrap\Model
+ * @license  MIT License https://opensource.org/licenses/MIT
+ * @author   Robert Lord, Codepeak AB <robert@codepeak.se>
+ * @link     https://codepeak.se
+ */
+class Transport extends \Zend_Mail_Transport_Smtp implements \Magento\Framework\Mail\TransportInterface
+{
+    /**
+     * @var \Magento\Framework\Mail\MessageInterface
+     */
+    protected $_message;
+
+    /**
+     * Transport constructor.
+     *
+     * @param MessageInterface     $message
+     * @param ScopeConfigInterface $scopeConfig
+     *
+     * @throws \Exception
+     */
+    public function __construct(MessageInterface $message, ScopeConfigInterface $scopeConfig)
+    {
+        // Make sure we're enabled
+        if ($scopeConfig->getValue('mailtrap/general/enabled') == '1') {
+            // Assure message is an instance of Zend_Mail
+            if (!$message instanceof \Zend_Mail) {
+                throw new \InvalidArgumentException('The message should be an instance of \Zend_Mail');
+            }
+
+            // Setup SMTP details
+            $smtpHost = 'smtp.mailtrap.io';
+            $smtpConf = [
+                'auth'     => 'login',
+                'tsl'      => 'tsl',
+                'port'     => '2525',
+                'username' => $scopeConfig->getValue('mailtrap/general/username'),
+                'password' => $scopeConfig->getValue('mailtrap/general/password')
+            ];
+
+            parent::__construct($smtpHost, $smtpConf);
+        }
+        $this->_message = $message;
+    }
+
+    /**
+     * Send a mail using this transport
+     * @return void
+     * @throws \Magento\Framework\Exception\MailException
+     */
+    public function sendMessage()
+    {
+        try {
+            parent::send($this->_message);
+        } catch (\Exception $e) {
+            throw new \Magento\Framework\Exception\MailException(new \Magento\Framework\Phrase($e->getMessage()), $e);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# magento2-mailtrap
+
+Integrate [mailtrap.io](https://mailtrap.io) with Magento 2 to simplify e-mail testing.
+
+## How to install
+
+After installing Magento 2, run the following commands from your Magento 2 root directory:
+
+```
+composer require codepeak/magento2-mailtrap
+php bin/magento cache:flush
+```
+
+## Contribute
+
+Feel free to **fork** and contribute to this module. Simply create a pull request and we'll review and merge your changes to master branch.
+
+## About Codepeak
+
+Codepeak is a Magento consultant agency located in Sweden. For more information, please visit [codepeak.se](https://codepeak.se).
